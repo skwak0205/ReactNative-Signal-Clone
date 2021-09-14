7e07e2c0db2c76f5890a1f93f34f0d040b6fa645
Soojung Kwak <gsj2464@gmail.com>
2021-09-14 13:12:19 +0900

    navigation 추가

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    </NavigationContainer>
   );
 }
 
---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCHEvCT4F7xw1qqkBpJhSlA_iLZu26zHpM',
+  authDomain: 'react-native-signal-cd668.firebaseapp.com',
+  projectId: 'react-native-signal-cd668',
+  storageBucket: 'react-native-signal-cd668.appspot.com',
+  messagingSenderId: '874594606769',
+  appId: '1:874594606769:web:ffb4c23c8b4fc7e4978c90',
+};
